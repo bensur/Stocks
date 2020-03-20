14a750b1fa5a97d8acfe5bf1e4b23fc4aa6ac46c
Ben Surkiss <bensur8@gmail.com>
2020-03-20 16:09:54 +0200

    Changed all responses to json, add help message and fixed validate_balance to support 'add' and 'sub' operations

--- management/StocksManager.py
+++ management/StocksManager.py
@@ -1,6 +1,7 @@
 import lib.helpers as helpers
 from management.DBManager import DBManager
 import json
+import re
 from alpha_vantage.timeseries import TimeSeries
 
 
@@ -8,13 +9,27 @@ class StocksManager:
     def __init__(self):
         self.logger = helpers.get_logger()
         self.supported_paths = {
-            "GET": ['account_summary'],
+            "GET": ['account_summary', 'help'],
             "POST": ['register', 'buy', 'sell']
         }
+        self.paths_help_message = {
+            "account_summary": "GET Method. Returns the account summary. No query params needed.",
+            "register": "POST Method. Register new user and return the generated token. POST body must contain username"
+                        " and initial deposit",
+            "buy": "POST Method. Buys stocks. "
+                   "POST body must contain 'stocks' hash where the key is the name of stock and"
+                   " the value is the number of stocks to buy",
+            "sell": "POST Method. Sells stocks. "
+                    "POST body must contain 'stocks' hash where the key is the name of stock to sell and"
+                   " the value is the number of stocks to sell"
+        }
+        for http_method in self.supported_paths.keys():
+            for path in self.supported_paths[http_method]:
+                if path not in self.paths_help_message:
+                    self.logger.warn("Could not find help message for path '%s'" % path)
         self.requests_counter = 0
         self.DBM = DBManager()
         self.ts = TimeSeries(key="TEFK1KZP8TU5E11J")  # TODO: Fix hardcoded key
-        # self.ts = TimeSeries()
 
     def handle(self, path, headers, command, post_data=None):
         self.requests_counter += 1
@@ -26,23 +41,46 @@ class StocksManager:
                                                                                      "post_data": post_data}))
         if command in self.supported_paths and path and \
            path.split("/")[1].split("?")[0] not in self.supported_paths[command]:
-            return {"response": "Error! path '" + str(path.split("/")[1].split("?")[0]) +
-                    "' is not supported for '" + str(command) + "'",
+            return {"response": json.dumps({"response_message": "Error! path '" +
+                                                                str(path.split("/")[1].split("?")[0]) +
+                                                                "' is not supported for '" +
+                                                                str(command) + "'"}),
                     "response_code": 400}
         if command == "POST":
-            if "register" in path:
+            if re.match(r'^/register', path):
                 return self.register(headers, post_data)
-            elif "buy" in path:
+            elif re.match(r'^/buy', path):
                 return self.update(headers, post_data, 'add')
-            elif "sell" in path:
+            elif re.match(r'^/sell', path):
                 return self.update(headers, post_data, 'sub')
+            else:
+                return {"response": json.dumps({"response_message": "Error! command '%s' not supported!" % command}),
+                        "response_code": 400}
         elif command == "GET":
             query_params = self.get_query_params(path)
-            if 'account_summary' in path:
+            if re.match(r'^/account_summary', path):
                 return self.get_account_summary(headers, query_params)
-            pass
+            elif re.match(r'^/help', path):
+                help_message = self.get_help_message()
+                return {"response": json.dumps({"response_message": help_message}),
+                        "response_code": 200}
+            else:
+                return {"response": json.dumps({"response_message": "Error! command '%s' not supported!" % command}),
+                        "response_code": 400}
         else:
-            return {"response": "Error! command '%s' not supported!" % command, "response_code": 400}
+            return {"response": json.dumps({"response_message": "Error! command '%s' not supported!" % command}),
+                    "response_code": 400}
+
+    def get_help_message(self):
+        docs_url = "https://docs.yourfriendlystocksexchange.com"
+        help_message = "Welcome to your friendly stocks exchange!\n" \
+                       "In order to use the service you need to use the register API (explained later on). " \
+                       "The register API return a toket you must use in all future API request in " \
+                       "HTTP header called 'token' (ie ['token': '<THE_TOKEN_RECEIVED>'.\n We support the following APIs:\n"
+        for path in self.paths_help_message.keys():
+            help_message += "\t%s - %s\n" % (path, self.paths_help_message[path])
+        help_message += "\nYou can find the full documentation at %s" % docs_url
+        return help_message
 
     @staticmethod
     def get_query_params(path):
@@ -68,46 +106,46 @@ class StocksManager:
     def register(self, headers, post_data):
         self.logger.debug("Got register request")
         if 'username' not in post_data.keys():
-            return {"response": "username not sent in post body",
+            return {"response": json.dumps({"response_message": "username not sent in post body"}),
                     "response_code": 400}
         else:
             user = post_data["username"]
         if 'deposit' not in post_data.keys():
-            return {"response": "you must make initial deposit!",
+            return {"response": json.dumps({"response_message": "you must make initial deposit!"}),
                     "response_code": 400}
         else:
             deposit = post_data["deposit"]
         token = self.create_new_user(user, deposit)
-        return {"response": "Here is your token: %s" % token,
+        return {"response": json.dumps({"response_message": "Here is your token: %s" % token}),
                 "response_code": 200}
 
-    def update(self, headers, post_data, operation):
+    def update(self, headers, post_data, stocks_operation):
         user_id = self.validate_token(headers)
         if not post_data:
             self.logger.warn("No post data for request #%i" % self.requests_counter)
-            return {"response": "No post data for request",
+            return {"response": json.dumps({"response_message": "No post data for request"}),
                     "response_code": 400}
         if user_id:
             if 'stocks' not in post_data:
                 self.logger.warn("User %s did not send stocks in the request" % user_id)
-                self.logger.debug("Post data sent (by user %s: %s" % (user_id, json.dumps(post_data)))
-                return {"response": "Request data does not contain 'stocks'!",
+                self.logger.debug("Post data sent by user %s: %s" % (user_id, json.dumps(post_data)))
+                return {"response": json.dumps({"response_message": "Request data does not contain 'stocks'!"}),
                         "response_code": 400}
             stocks = post_data["stocks"]
             self.logger.debug("User %s requested to buy %s" % (user_id, json.dumps(stocks)))
-            balance = self.validate_balance(user_id, stocks)
+            balance = self.validate_balance(user_id, stocks, stocks_operation)
             if not balance:
                 response_message = "User %s does not have enough in his balance to buy stocks %s" % (user_id,
                                                                                                      json.dumps(stocks))
                 self.logger.warn(response_message)
-                return {"response": response_message,
+                return {"response": json.dumps({"response_message": response_message}),
                         "response_code": 400}
-            self.DBM.update(user_id, balance, stocks, operation)
-            return {"response": "Updated stocks!",
+            self.DBM.update(user_id, balance, stocks, stocks_operation)
+            return {"response": json.dumps({"response_message": "Updated stocks!"}),
                     "response_code": 200}
         else:
             self.logger.warn("No valid token in request #%i" % self.requests_counter)
-            return {"response": "No valid token received",
+            return {"response": json.dumps({"response_message": "No valid token received"}),
                     "response_code": 401}
 
     def get_account_summary(self, headers, query_params):
@@ -121,7 +159,7 @@ class StocksManager:
                     "response_code": 200}
         else:
             self.logger.warn("No valid token in request #%i" % self.requests_counter)
-            return {"response": "No valid token received",
+            return {"response": json.dumps({"response_message": "No valid token received"}),
                     "response_code": 401}
 
     def validate_token(self, headers):
@@ -138,16 +176,21 @@ class StocksManager:
         self.logger.debug("Created new user %s" % json.dumps(user))
         return user["token"]
 
-    def validate_balance(self, user_id, stocks):
+    def validate_balance(self, user_id, stocks, stocks_operation):
         total = 0
         for stock in stocks:
             total += self.get_stock_price(stock["name"]) * stock["amount"]
         user = self.DBM.find(user_id)
         cur_balance = user["balance"]
-        if cur_balance > total:
-            return cur_balance - total
+        if stocks_operation == "add":
+            if cur_balance > total:
+                return cur_balance - total
+            else:
+                return None
+        elif stocks_operation == "sub":
+            return cur_balance + total
         else:
-            return None
+            self.logger.error("stocks_operation '%s' is not supported in validate_balance function")
 
     def get_stock_price(self, stock_name):
         data, meta_data = self.ts.get_intraday(stock_name)  # TODO: check the stock exist?
